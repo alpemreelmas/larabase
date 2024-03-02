7c846acfd4a54973126111ef91ae2dd8d08cee1f
alpemreelmas <elmasalpemre@gmail.com>
2024-03-02 20:36:50 +0100

    feat: add module installer

--- Modules/User/resources/views/components/dataTable-css.blade.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{asset("aee/")}}/plugins/src/table/datatable/datatables.css">
-<link rel="stylesheet" type="text/css" href="{{asset("aee/")}}/plugins/css/light/table/datatable/dt-global_style.css">
-<link rel="stylesheet" type="text/css" href="{{asset("aee/")}}/plugins/css/dark/table/datatable/dt-global_style.css">
-<link href="{{asset("aee/")}}/layouts/vertical-dark-menu/css/dark/structure-mod.css" rel="stylesheet" type="text/css" />
-<link href="{{asset("aee/")}}/layouts/vertical-dark-menu/css/light/structure-mod.css" rel="stylesheet" type="text/css" />
